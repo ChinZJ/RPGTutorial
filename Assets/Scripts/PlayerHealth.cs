cb066b72d8fa0c4db5584f8179988d1a096737b3
ChinZJ <zhenjie.chin88@gmail.com>
2025-05-03 01:51:57 +0800

    Add Player health bar UI and animation

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 /** Controls health logic. */
@@ -7,10 +8,21 @@ public class PlayerHealth : MonoBehaviour
     public int currentHealth;
     public int maxHealth;
 
+    public TMP_Text healthText;
+    public Animator healthTextAnim;
+
+    private void Start()
+    {
+        healthText.text = "HP: " + currentHealth + " / " + maxHealth;
+    }
+
     /** Changes the health of the object. */
     public void ChangeHealth(int amount)
     {
         currentHealth += amount;
+
+        healthTextAnim.Play("TextUpdate");
+        healthText.text = "HP: " + currentHealth + " / " + maxHealth;
 
         // Object dies.
         if (currentHealth <= 0)
