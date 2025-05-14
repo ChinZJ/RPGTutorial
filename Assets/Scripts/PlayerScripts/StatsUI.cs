f729a0d12f7902c2237fabae23d987fd30285efd
ChinZJ <zhenjie.chin88@gmail.com>
2025-05-14 19:14:57 +0800

    Add toggle function to StatsUI in menu

--- Assets/Scripts/PlayerScripts/StatsUI.cs
+++ Assets/Scripts/PlayerScripts/StatsUI.cs
@@ -1,8 +1,54 @@
+using TMPro;
 using UnityEngine;
 
 public class StatsUI : MonoBehaviour
 {
     public GameObject[] statsSlots;
+    public CanvasGroup statsCanvas;
 
-    
+    private bool statsOpen = false;
+
+    private void Start()
+    {
+        UpdateAllStats();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("ToggleStats"))
+        {
+            if (statsOpen)
+            {
+                Time.timeScale = 1;
+                UpdateAllStats();
+                statsCanvas.alpha = 0;
+                statsOpen = false;
+            } else 
+            {
+                Time.timeScale = 0;
+                UpdateAllStats();
+                statsCanvas.alpha = 1;
+                statsOpen = true;
+            }
+            
+        }
+    }
+
+    public void UpdateAllStats()
+    {
+        UpdateDamage();
+        UpdateSpeed();
+    }
+
+    public void UpdateDamage()
+    {
+        statsSlots[0].GetComponentInChildren<TMP_Text>(). text = "Damage: "
+                + StatsManager.Instance.damage;
+    }
+
+    public void UpdateSpeed()
+    {
+        statsSlots[1].GetComponentInChildren<TMP_Text>(). text = "Speed: "
+                + StatsManager.Instance.speed;
+    }
 }
