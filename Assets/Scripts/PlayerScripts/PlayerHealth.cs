15da757caba113a81a23dc5e4d66ef362a7cc319
ChinZJ <zhenjie.chin88@gmail.com>
2025-05-14 18:44:34 +0800

    Add StatsManager.cs to control Player stats

--- Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -4,29 +4,27 @@ using UnityEngine;
 /** Controls health logic. */
 public class PlayerHealth : MonoBehaviour
 {
-    
-    public int currentHealth;
-    public int maxHealth;
-
     public TMP_Text healthText;
     public Animator healthTextAnim;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created.
     private void Start()
     {
-        healthText.text = "HP: " + currentHealth + " / " + maxHealth;
+        healthText.text = "HP: " + StatsManager.Instance.currentHealth
+                + " / " + StatsManager.Instance.maxHealth;
     }
 
     /** Changes the health of the object. */
     public void ChangeHealth(int amount)
     {
-        currentHealth += amount;
+        StatsManager.Instance.currentHealth += amount;
 
         healthTextAnim.Play("TextUpdate");
-        healthText.text = "HP: " + currentHealth + " / " + maxHealth;
+        healthText.text = "HP: " + StatsManager.Instance.currentHealth
+                + " / " + StatsManager.Instance.maxHealth;
 
         // Object dies.
-        if (currentHealth <= 0)
+        if (StatsManager.Instance.currentHealth <= 0)
         {
             gameObject.SetActive(false); // gameObject refers to object script is on.
         }
